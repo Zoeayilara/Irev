ef0752f48f62b4e64bfa6bc95c1d1306518a98cd
Zoeayilara <zoeayilara@gmail.com>
2026-02-28 13:30:11 +0100

    databse url

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@prisma/client'],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["irevv.netlify.app", "*.netlify.app", "localhost:3000"]
+    }
+  }
 };
 
 export default nextConfig;
